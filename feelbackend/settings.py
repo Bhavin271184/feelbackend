b7fd557501e95eefae97d02db7e18aa785fb5454
gnanesh <gnaneshmurthy5@gmail.com>
2024-08-24 12:28:21 +0530

    sto

--- feelbackend/settings.py
+++ feelbackend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
